b9e3a185f4202e1e667ab05f8de6bd59b4c0f6ee
Stuart Hanberg <shanberg@users.noreply.github.com>
2024-08-05 19:29:59 -0400

    update installer

--- main.sh
+++ main.sh
@@ -1,16 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# List of tools and their installation commands
+# Associative array of tool names and their installation commands
 declare -A tools=(
-    ["now"]="curl -fsSL https://raw.githubusercontent.com/shanberg/now/main/dist/install.sh | sh"
-    ["pre"]="curl -fsSL https://raw.githubusercontent.com/shanberg/pre/main/install.sh | sh"
+    ["now"]="curl -fsSL https://raw.githubusercontent.com/shanberg/now/main/dist/install.sh | bash"
+    ["pre"]="curl -fsSL https://raw.githubusercontent.com/shanberg/pre/main/install.sh | bash"
 )
-
-# Get install command for a tool
-get_install_command() {
-    local tool_name="$1"
-    echo "${tools[$tool_name]}"
-}
 
 # Check if a tool is installed
 is_tool_installed() {
@@ -19,18 +13,18 @@ is_tool_installed() {
 
 install_tool() {
     TOOL_NAME=$1
-    install_command=$(get_install_command "$TOOL_NAME")
+    install_command="${tools[$TOOL_NAME]}"
     if [ -z "$install_command" ]; then
         echo "Tool $TOOL_NAME not found."
         return
     fi
 
-    echo "Found tool: $TOOL_NAME"
     if is_tool_installed "$TOOL_NAME"; then
         echo "Tool $TOOL_NAME is already installed."
-        echo -n "Reinstall it? (y/n) "
+        echo -n "Would you like to reinstall it? (y/n) "
         read choice < /dev/tty
         if [ "$choice" != "y" ]; then
+            echo "Skipping reinstallation of $TOOL_NAME."
             return
         fi
     fi
@@ -39,23 +33,22 @@ install_tool() {
 }
 
 interactive_mode() {
-    tool_names=("${!tools[@]}" "Quit")
+    tools_list=("${!tools[@]}" "Quit")
 
     while true; do
         echo "Please select a tool to install:"
-        for i in "${!tool_names[@]}"; do
-            echo "$((i+1))) ${tool_names[$i]}"
+        for i in "${!tools_list[@]}"; do
+            echo "$((i+1))) ${tools_list[$i]}"
         done
 
         read -p "Enter the number of your choice: " choice < /dev/tty
-        if [[ "$choice" -ge 1 && "$choice" -le "${#tool_names[@]}" ]]; then
-            tool="${tool_names[$((choice-1))]}"
+        if [[ "$choice" -ge 1 && "$choice" -le "${#tools_list[@]}" ]]; then
+            tool="${tools_list[$((choice-1))]}"
             if [ "$tool" == "Quit" ]; then
                 echo "Exiting."
                 break
             else
                 install_tool "$tool"
-                break
             fi
         else
             echo "Invalid selection."
